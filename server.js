de6ab24e73ea62161081f31e17eef70b432ed30d
Júlia Kröger <julia.kroger@gmail.com>
2020-02-18 10:48:36 -0300

    feat: add lint to project

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const app = express();
-app.use(express.json({ extended: false }));
+const express = require('express')
+const app = express()
+app.use(express.json({ extended: false }))
 
-app.get('/', (req, res) => res.send('API running'));
+app.get('/', (req, res) => res.send('API running'))
 
-app.listen(process.env.PORT || 5000, () => console.log(`Server running`));
+app.listen(process.env.PORT || 5000, () => console.log('Server running'))
